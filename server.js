6251660fc9c50a499f53ae05b0ebb43ae62e17f9
Donald McKendrick <ddmckendrick@gmail.com>
2015-04-15 15:30:31 +0100

    add scrapers for HOF and JL

--- server.js
+++ server.js
@@ -14,10 +14,15 @@ app.get('/favicon.ico', function(req, res){
   console.log("No favicon to find here!");
 })
 
+app.get('/', function(req, res){
+  res.send("Place a product url after the url to scrape for sizes!");
+})
+
 app.get('*', function(req, res){
   var url = req.originalUrl.substring(1);
   console.log("Scraping: " + url);
-  if (url !== "favicon.ico") {
+  if (url.indexOf("www.asos.com") !== -1) {
+    console.log("scraping an asos product"); 
     phantom.create(function(ph) {
       return ph.createPage(function(page) {
         return page.open(url, function(status) {
@@ -58,6 +63,80 @@ app.get('*', function(req, res){
             });
         });
     });
+  } else if (url.indexOf("www.houseoffraser.co.uk") !== -1) {
+    console.log("Scraping a House of Fraser Product")
+    phantom.create(function(ph) {
+      return ph.createPage(function(page) {
+        return page.open(url, function(status) {
+          console.log("opened site? ", status);         
+     
+                page.injectJs('http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js', function() {
+                    //jQuery Loaded.
+                    //Wait for a bit for AJAX content to load on the page. Here, we are waiting 5 seconds.
+                    setTimeout(function() {
+                        return page.evaluate(function() {
+     
+                            //Get what you want from the page using jQuery. A good way is to populate an object with all the jQuery commands that you need and then return the object.
+                            var sArr = [];
+
+                            $('.size-swatches-list li:not(.disabled) a').each(function() {
+                                sArr.push($(this).html());
+                            });
+     
+                            return {
+                                sizes: sArr
+                            };
+                        }, function(result) {
+                            result.sizes = _.map(result.sizes, function(n) {
+                              return {name: n}
+                            });
+                            res.send(result);
+                            ph.exit();
+                        });
+                    }, 100);
+     
+                });
+            });
+        });
+    });
+  } else if (url.indexOf("www.johnlewis.com") !== -1) {
+    console.log("Scraping a John Lewis Product");
+    phantom.create(function(ph) {
+      return ph.createPage(function(page) {
+        return page.open(url, function(status) {
+          console.log("opened site? ", status);         
+     
+                page.injectJs('http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js', function() {
+                    //jQuery Loaded.
+                    //Wait for a bit for AJAX content to load on the page. Here, we are waiting 5 seconds.
+                    setTimeout(function() {
+                        return page.evaluate(function() {
+     
+                            //Get what you want from the page using jQuery. A good way is to populate an object with all the jQuery commands that you need and then return the object.
+                            var sArr = [];
+
+                            $('#prod-product-size ul.selection-grid li:not(.out-of-stock) a span').each(function() {
+                                sArr.push($(this).html());
+                            });
+     
+                            return {
+                                sizes: sArr
+                            };
+                        }, function(result) {
+                            result.sizes = _.map(result.sizes, function(n) {
+                              return {name: n}
+                            });
+                            res.send(result);
+                            ph.exit();
+                        });
+                    }, 100);
+     
+                });
+            });
+        });
+    });
+  } else {
+    res.send("cant scrape this currently");
   }
 
 });
